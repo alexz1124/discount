413114b8e00146b1dad61cdd49e384d23fe69de5
alexz1124 <alexz.waranyu@gmail.com>
2019-10-31 01:16:21 +0700

    share

--- app/src/main/java/buu/informatics/s59160129/discountandcoupon/AboutFragment.kt
+++ app/src/main/java/buu/informatics/s59160129/discountandcoupon/AboutFragment.kt
@@ -1,13 +1,13 @@
 package buu.informatics.s59160129.discountandcoupon
 
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.FrameMetrics
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import buu.informatics.s59160129.discountandcoupon.databinding.FragmentAboutBinding
 
 /**
@@ -26,6 +26,30 @@ class AboutFragment : Fragment() {
             container,
             false
         )
+        setHasOptionsMenu(true)
         return binding.root
+    }
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.share_menu, menu)
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun getShareIntent() : Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text))
+        return shareIntent
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
     }
 }
